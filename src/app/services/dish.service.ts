cb483c1330dde61616243dd7a43984b71a2d4e7e
neal <anilksee@gmail.com>
2017-08-15 22:52:10 -0400

    RxJS - Observable

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,34 +1,54 @@
 import { Injectable } from '@angular/core';
 import { DISHES } from '../shared/dishes';
 import { Dish } from '../shared/dish';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]> {
-    //return Promise.resolve(DISHES);
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES), 2000);
-    });
-  }
+  // getDishes(): Promise<Dish[]> {
+  //   //return Promise.resolve(DISHES);
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(DISHES), 2000);
+  //   });
+  // }
+  //
+  // getDish(id: number): Promise<Dish> {
+  //   //return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+  //   });
+  // }
+  //
+  // getFeaturedDish(): Promise<Dish> {
+  //   //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+  //   return  new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+  //   });
+  // }
 
-  getDish(id: number): Promise<Dish> {
-    //return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
-    });
-  }
+  getDishes(): Observable<Dish[]> {
+   return Observable.of(DISHES).delay(2000);
+ }
 
-  getFeaturedDish(): Promise<Dish> {
-    //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    });
+ getDish(id: number): Observable<Dish> {
+   return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
+ }
+
+ getFeaturedDish(): Observable<Dish> {
+   return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000);
+ }
+
+ getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id ));
   }
 
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,34 +1,51 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class LeaderService {
 
   constructor() { }
 
-  getLeaders() : Promise<Leader[]>{
-    //return Promise.resolve(LEADERS);
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS), 2000);
-    });
+  // getLeaders() : Promise<Leader[]>{
+  //   //return Promise.resolve(LEADERS);
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(LEADERS), 2000);
+  //   });
+  // }
+  //
+  // getLeader(id: number): Promise<Leader> {
+  //   //return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+  //   });
+  // }
+  //
+  // getFeaturedLeader(): Promise<Leader> {
+  //   //return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  //   return  new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //       setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+  //   });
+  // }
+
+
+  getLeaders() : Observable<Leader[]>{
+    return Observable.of(LEADERS).delay(2000);
   }
 
-  getLeader(id: number): Promise<Leader> {
-    //return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeader(id: number): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000);
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    //return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-    });
+  getFeaturedLeader(): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
   }
 
 }
