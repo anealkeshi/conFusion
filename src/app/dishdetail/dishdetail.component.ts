fbe016eba2993ca50476e32c2edeb764f3ce91c3
neal <anilksee@gmail.com>
2017-08-17 21:37:49 -0400

    Angular animation

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { baseURL } from '../shared/baseurl';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -11,7 +12,20 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 export class DishdetailComponent implements OnInit {
@@ -21,6 +35,8 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
   dishErrMess: string;
+  visibility = 'shown';
+  errMess : string;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -32,8 +48,9 @@ export class DishdetailComponent implements OnInit {
                       .subscribe(dishIds => this.dishIds = dishIds,
                                 errmess => this.dishErrMess = <any>errmess);
       this.route.params
-        .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+        .switchMap((params: Params) => {this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); })
+        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id);  this.visibility = 'shown'; },
+          errmess => { this.dish = null; this.errMess = <any>errmess; });
     }
 
     setPrevNext(dishId: number) {
